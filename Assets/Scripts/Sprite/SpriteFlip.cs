d471ebce90102d94ee32357792dca3943acd5be9
Christian Oliveros <christianol_01@hotmail.com>
2019-01-08 23:17:13 -0400

    SpriteFlip

--- Assets/Scripts/Sprite/SpriteFlip.cs
+++ Assets/Scripts/Sprite/SpriteFlip.cs
@@ -2,17 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
 public class SpriteFlip : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Ecala cuando esta normal el sprite
+    /// </summary>
+    private Vector2 normalScale = new Vector2(1.0f, 1.0f);
+
+    /// <summary>
+    /// Ecala cuando esta invertido el sprite
+    /// </summary>
+    private Vector2 flippedScale = new Vector2(-1.0f, 1.0f);
+
+    /// <summary>
+    /// Offset de sprite en estado normal
+    /// </summary>
+    private Vector2 normalOffset = new Vector2(0, 0);
+
+    /// <summary>
+    /// Offset de sprite en estado invertido
+    /// </summary>
+    private Vector2 flippedOffset = new Vector2(1.0f, 0);
+
+    /// <summary>
+    /// Renderer del sprite
+    /// </summary>
+    Renderer rend;
+
+    /// <summary>
+    /// Si se esta invirtiendo x
+    /// </summary>
+    private bool _flipX = false;
+
+    /// <summary>
+    /// Si se esta invirtiendo x
+    /// </summary>
+    public bool FlipX { get { return _flipX; } set { ChangeFlipState(value); } }
+
+    /// <summary>
+    /// Cambiar el estado del flip a state
+    /// /// </summary>
+    /// <param name="state">Nuevo estado del flip</param>
+    public void ChangeFlipState(bool state)
     {
-        
+        _flipX = state;
+        if (_flipX)
+        {
+            rend.material.mainTextureScale = flippedScale;
+            rend.material.mainTextureOffset = flippedOffset;
+        }
+        else
+        {
+            rend.material.mainTextureScale = normalScale;
+            rend.material.mainTextureOffset = normalOffset;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        rend = GetComponent<Renderer>();
     }
 }
