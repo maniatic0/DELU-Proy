6068c4a947db2fbe174fb6469cb6c3cf695f9dfe
Christian Oliveros <christianol_01@hotmail.com>
2019-01-24 15:25:41 -0400

    Separated Humanoid Movement Animation from Humanoid Movement

--- Assets/Scripts/Movement/HumanoidMovement.cs
+++ Assets/Scripts/Movement/HumanoidMovement.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(SpriteFlip))]
-public class HumanoidMovement : MonoBehaviour {
+public class HumanoidMovement : MonoBehaviour
+{
     /// <summary>
 	/// Velocidad maxima a la que ira el humanoide
 	/// </summary>
-    [Tooltip("Velocidad maxima a la que ira el humanoide")]   
-	[SerializeField] 
+    [Tooltip("Velocidad maxima a la que ira el humanoide")]
+    [SerializeField]
     protected float maxSpeed = 7f;
 
     /// <summary>
@@ -26,25 +26,19 @@ public class HumanoidMovement : MonoBehaviour {
 	private Rigidbody rb;
 
     /// <summary>
-    /// Componente para hacer flip de humanoide
-    /// </summary>
-	private SpriteFlip spriteFlip;
-
-    /// <summary>
-    /// Si el humanoide esta viendo a la derecha
-    /// Se usa para no hacer flip si esta viendo a la izquiera con cero velocidad
-    /// </summary>
-	private bool isFacingRight = true;
-
-    /// <summary>
     /// Velocidad actual
     /// </summary>
     private Vector3 vel = Vector3.zero;
 
-	void Awake() {
-		rb = GetComponent <Rigidbody> ();
-		spriteFlip = GetComponent <SpriteFlip> ();
-	}
+    /// <summary>
+    /// Velocidad actual
+    /// </summary>
+    public Vector3 Velocity { get { return vel; } }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     /// <summary>
     /// Poner la velocidad actual de humanoide
@@ -53,7 +47,8 @@ public class HumanoidMovement : MonoBehaviour {
     /// <param name="vel_x">Velocidad en X</param>
     /// <param name="vel_y">Velocidad en Y</param>
     /// <param name="vel_z">Velocidad en Z</param>
-    protected void SetVelocity(float vel_x, float vel_y, float vel_z) {
+    protected void SetVelocity(float vel_x, float vel_y, float vel_z)
+    {
         vel.x = vel_x;
         vel.y = vel_y;
         vel.z = vel_z;
@@ -64,40 +59,29 @@ public class HumanoidMovement : MonoBehaviour {
     /// Si la velocidad esta sobre el maximo, esta se recorta al maximo
     /// </summary>
     /// <param name="new_vel">Nueva velocidad</param>
-    protected void SetVelocity(Vector3 new_vel) {
+    protected void SetVelocity(Vector3 new_vel)
+    {
         vel = new_vel;
     }
 
-	void FixedUpdate() {
-		Move();
-		Flip();
-	}
+    void FixedUpdate()
+    {
+        Move();
+    }
 
     /// <summary>
     /// Coloca la Velocidad al humanoide
     /// Si la velocidad esta sobre el maximo, esta se recorta al maximo
     /// </summary>
-	void Move() {
+	void Move()
+    {
         maxSpeedSqr = maxSpeed * maxSpeed;
         curSpeedSqr = vel.sqrMagnitude;
         if (maxSpeedSqr < curSpeedSqr)
         {
             vel = (vel / Mathf.Sqrt(curSpeedSqr)) * maxSpeed;
         }
-		rb.velocity = vel;
-	}
-
-    /// <summary>
-    /// Voltea el sprite del humanoide a la direccion del movimiento
-    /// </summary>
-	void Flip() {
-		if(rb.velocity.x < 0 && isFacingRight) {
-            spriteFlip.FlipX = true;
-            isFacingRight = false;
-        } else if(rb.velocity.x > 0 && !isFacingRight) {
-            spriteFlip.FlipX = false;
-            isFacingRight = true;
-        }
-	}
+        rb.velocity = vel;
+    }
 
 }
---
+++ Assets/Scripts/Movement/HumanoidMovementAnimation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HumanoidMovement))]
+[RequireComponent(typeof(SpriteFlip))]
+public class HumanoidMovementAnimation : MonoBehaviour
+{
+
+    /// <summary>
+    /// Componente para hacer flip de humanoide
+    /// </summary>
+	private SpriteFlip spriteFlip;
+
+    /// <summary>
+    /// Componente de manejo de movimiento
+    /// </summary>
+    private HumanoidMovement humanoidMovement;
+
+    /// <summary>
+    /// Si el humanoide esta viendo a la derecha
+    /// Se usa para no hacer flip si esta viendo a la izquiera con cero velocidad
+    /// </summary>
+	private bool isFacingRight = true;
+
+
+    void Awake()
+    {
+        spriteFlip = GetComponent <SpriteFlip> ();
+        humanoidMovement = GetComponent<HumanoidMovement>();
+    }
+
+    /// <summary>
+    /// LateUpdate is called every frame, if the Behaviour is enabled.
+    /// It is called after all Update functions have been called.
+    /// </summary>
+    void LateUpdate()
+    {
+        Flip();
+    }
+
+    /// <summary>
+    /// Voltea el sprite del humanoide a la direccion del movimiento
+    /// </summary>
+	void Flip() {
+		if(humanoidMovement.Velocity.x < 0 && isFacingRight) {
+            spriteFlip.FlipX = true;
+            isFacingRight = false;
+        } else if(humanoidMovement.Velocity.x > 0 && !isFacingRight) {
+            spriteFlip.FlipX = false;
+            isFacingRight = true;
+        }
+	}
+}
